e217593049c5458104afbd3f08253fefc49e2ddd
Sanya000 <103216915+Sanya000@users.noreply.github.com>
2023-04-29 19:32:59 +0100

    Linear_regression.v

--- VHDL Code/Linear_regression.v
+++ VHDL Code/Linear_regression.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 20.03.2022 20:39:22
-// Design Name: 
-// Module Name: Linear_regression
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module Linear_regression(
