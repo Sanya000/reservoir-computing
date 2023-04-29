e7c5cf234f8354d67b8200d05489a510652ccf60
Sanya000 <103216915+Sanya000@users.noreply.github.com>
2023-04-29 19:36:43 +0100

    mux_2.v

--- VHDL Code/mux_2.v
+++ VHDL Code/mux_2.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.03.2022 01:21:57
-// Design Name: 
-// Module Name: mux_2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mux_2( //multiplexer using a binary 1 or 0 clock and 2 inputs
     input clk,
