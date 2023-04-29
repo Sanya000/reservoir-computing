18b74a73cc1b99603ada64e18952599001b6a62d
Sanya000 <103216915+Sanya000@users.noreply.github.com>
2023-04-29 19:31:23 +0100

    Update Activation_function.v

--- VHDL Code/Activation_function.v
+++ VHDL Code/Activation_function.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.03.2022 21:14:47
-// Design Name: 
-// Module Name: Memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Activation_function(//3piecewise linear approximation of the tanh function
     input wire [16:0] x,
