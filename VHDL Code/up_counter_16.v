cb665b208dfa4aeb64ae54846a100890bc887d07
Sanya000 <103216915+Sanya000@users.noreply.github.com>
2023-04-29 19:37:28 +0100

    up_counter_16.v

--- VHDL Code/up_counter_16.v
+++ VHDL Code/up_counter_16.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.03.2022 23:03:22
-// Design Name: 
-// Module Name: up_counter_16
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module up_counter_16(
