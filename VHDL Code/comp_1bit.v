661386bc1838d193bf78e52d80a1d04b5a0a5251
Sanya000 <103216915+Sanya000@users.noreply.github.com>
2023-04-29 19:36:04 +0100

    comp_1bit.v

--- VHDL Code/comp_1bit.v
+++ VHDL Code/comp_1bit.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.03.2022 22:03:04
-// Design Name: 
-// Module Name: comp_4bit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module comp_1bit(//comparator module for 1bit numbers which tells which is greater or if they are equal using gate logic
     input wire a,
