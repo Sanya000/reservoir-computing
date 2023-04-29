e2f9bd0fcc7313aa7b25ada56046736d2c6e8a67
Sanya000 <103216915+Sanya000@users.noreply.github.com>
2023-04-29 19:35:03 +0100

    Reservoir_computer.v

--- VHDL Code/Reservoir_computer.v
+++ VHDL Code/Reservoir_computer.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.03.2022 21:14:47
-// Design Name: 
-// Module Name: Memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module Reservoir_computer(
