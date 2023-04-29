8376e75e8beeb30e7a2e441c6265910b5f90288f
Sanya000 <103216915+Sanya000@users.noreply.github.com>
2023-04-29 19:33:44 +0100

    Node.v

--- VHDL Code/Node.v
+++ VHDL Code/Node.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.03.2022 21:14:47
-// Design Name: 
-// Module Name: Memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Node( //two input node that does operations with stochastic bit streams and then converst back to binary for the rest of the computations
     input wire clk, Nc,
